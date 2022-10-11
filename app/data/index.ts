3312d186cd025c34e86b7a3d176fc5da9b432dbc
Leon Radley <leon@radley.se>
2022-10-11 20:25:06 +0200

    fix caching and remove old files from firebase deploy

--- app/data/index.ts
+++ app/data/index.ts
@@ -3,5 +3,5 @@
 //     ? 'public,max-age=3600,s-maxage=3600,stale-while-revalidate=86400,stale-if-error=86400'
 //     : 'private,no-cache'
 
-export const DEFAULT_CACHE =
-  'public,max-age=3600,s-maxage=3600,stale-while-revalidate=604800,stale-if-error=604800'
+// vercel max cache is 31 days = 2678400 seconds
+export const DEFAULT_CACHE = 'public,max-age=0,s-maxage=3600,stale-while-revalidate=2678400,stale-if-error=2678400'
